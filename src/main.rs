be10b4ad3443558257b65fb0d252b9ce9e6244d4
Ryan Ruckley <rruckley@gmail.com>
2023-09-19 09:56:06 +1000

    initial

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+
+use log::info;
+
+fn main() {
+    let pkg = env!("CARGO_PKG_NAME");
+    let ver = env!("CARGO_PKG_VERSION");
+
+    env_logger::init();
+
+    info!("Starting {pkg} v{ver}");
+}
