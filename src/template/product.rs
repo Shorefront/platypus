0d88f04c933f84f769263968f86b2b7a69d4f9d7
Ryan Ruckley <rruckley@gmail.com>
2023-09-19 14:22:37 +1000

    new template

--- src/template/product.rs
+++ src/template/product.rs
@@ -6,5 +6,14 @@ use serde::{Deserialize,Serialize};
 
 #[derive(Deserialize,Serialize)]
 pub struct ProductTemplate {
-    offering : ProductOffering,
+    offering    : Option<ProductOffering>,
+    components  : Option<Vec<super::component::ComponentTemplate>>,
+}
+
+impl ProductTemplate {\
+    pub fn new(name : String) -> ProductTemplate {
+        let offering = ProductOffering::new(name);
+        ProductTemplate { offering, components: None }
+    }
+
 }
