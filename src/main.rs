2d1f3a6e91d4b8937a02ca0afe6b0c551e4f6e0a
Ryan Ruckley <rruckley@gmail.com>
2023-09-19 16:56:31 +1000

    create sample objects

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod common;
 
 use common::config::Config;
 
+use crate::template::{product::ProductTemplate, component::ComponentTemplate};
+
 #[warn(missing_docs)]
 
 fn main() {
@@ -18,4 +20,11 @@ fn main() {
     info!("Starting {pkg} v{ver}");
 
     let _cfg = Config::new();
+
+    let component1 = ComponentTemplate::new(String::from("Access"));
+    let component2 = ComponentTemplate::new(String::from("Service"));
+    let template = ProductTemplate::new(String::from("Fixed_Internet_Template"));
+    let _result = template.add_components(&mut vec![component1,component2]);
+    //let _result = template.add_component(component2);
+    
 }
