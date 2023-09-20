651cf9e70e65d03988498b52c244b7976c3d3b36
Ryan Ruckley <rruckley@gmail.com>
2023-09-20 16:00:50 +1000

    use categories

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod template;
 mod common;
 
 use common::config::Config;
+use tmflib::tmf620::category::Category;
+use tmflib::tmf620::catalog::Catalog;
 
 use crate::template::{product::ProductTemplate, component::ComponentTemplate};
 
@@ -24,7 +26,12 @@ fn main() {
     let component1 = ComponentTemplate::new(String::from("Access"));
     let component2 = ComponentTemplate::new(String::from("Service"));
     let template = ProductTemplate::new(String::from("Fixed_Internet_Template"));
+    let catalog = Catalog::new().name(String::from("Templates"));
+    let access_category = Category::new(String::from("Access"));
     let _result = template.add_components(&mut vec![component1,component2]);
+    let _result = catalog.add_category(access_category);
+    let _result = template.with_category(access_category);
+    //let _result = catalog.add(template);
     //let _result = template.add_component(component2);
     
 }
