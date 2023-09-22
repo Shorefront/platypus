b175e7b0ad10b40cd3c5fc03c3be03132909188a
Ryan Ruckley <rruckley@gmail.com>
2023-09-22 18:16:19 +1000

    useful fields

--- src/model/component/product.rs
+++ src/model/component/product.rs
@@ -1,7 +1,21 @@
 //! Product Component
 //! 
 use serde::{Deserialize,Serialize};
+use tmflib::tmf620::product_specification::ProductSpecification;
+use tmflib::tmf620::product_offering::ProductOffering;
+
+use std::convert::Into;
+
 
 /// Product Component
 #[derive(Deserialize,Serialize)]
-pub struct ProductComponent {}
+pub struct ProductComponent {
+    offer   : ProductOffering,
+    specification : ProductSpecification,     
+}
+
+impl Into<ProductOffering> for ProductComponent {
+    fn into(self) -> ProductOffering {
+        self.offer
+    }
+}
