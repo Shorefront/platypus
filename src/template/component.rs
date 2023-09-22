6e0f7a00bce0b866fa7978b151c456c6d1b692ec
Ryan Ruckley <rruckley@gmail.com>
2023-09-22 18:15:21 +1000

    formatting

--- src/template/component.rs
+++ src/template/component.rs
@@ -3,13 +3,17 @@
 
 use serde::{Deserialize,Serialize};
 
+/// A Component Template defines how to build a component
 #[derive(Debug,Deserialize,Serialize)]
 pub struct ComponentTemplate {
     pub name : String,
 }
 
 impl ComponentTemplate {
+    /// Create a new component template
     pub fn new(name : String) -> ComponentTemplate {
-        ComponentTemplate { name  }
+        ComponentTemplate { 
+            name 
+        }
     }
 }
