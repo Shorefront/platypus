ac133d01d95ee77ba61da2a4ef4bd97b597cd977
Ryan Ruckley <rruckley@gmail.com>
2023-09-25 13:53:39 +1000

    conversion

--- src/model/component/product.rs
+++ src/model/component/product.rs
@@ -1,21 +1,49 @@
 //! Product Component
 //! 
 use serde::{Deserialize,Serialize};
-use tmflib::tmf620::product_specification::ProductSpecification;
+use tmflib::tmf620::category::{Category,CategoryRef};
 use tmflib::tmf620::product_offering::ProductOffering;
+use crate::template::component::ComponentTemplate;
 
 use std::convert::Into;
 
+use super::COMPONENT_CATEGORY;
+
 
 /// Product Component
-#[derive(Deserialize,Serialize)]
+#[derive(Clone,Debug,Deserialize,Serialize)]
 pub struct ProductComponent {
-    offer   : ProductOffering,
-    specification : ProductSpecification,     
+    offer   : ProductOffering,   
 }
 
+impl ProductComponent {
+    pub fn from_offer(offer : ProductOffering) -> ProductComponent {
+        ProductComponent {
+            offer
+        }
+    }
+}
+
+/// Create a ProductOffering from a ProductComponent for storage
 impl Into<ProductOffering> for ProductComponent {
+
     fn into(self) -> ProductOffering {
-        self.offer
+        
+        // Clone source template so we don't change it
+        let mut new_self = self.clone();
+        // When converting we need to change the category from 'Template' to 'Component'
+        let comp_cat = Category::new(COMPONENT_CATEGORY.to_string());
+        new_self.offer.category = Some(vec![CategoryRef::from(&comp_cat)]);
+        new_self.offer
+    }
+}
+
+
+// Instantiate a Product Component from a Product Template
+impl From<ComponentTemplate> for ProductComponent {
+    fn from(ct : ComponentTemplate) -> ProductComponent {
+         // Clone source template so we don't change it
+         let pc = ProductComponent::from_offer(ct.component.unwrap().offer);
+         pc
     }
 }
