b8c3d9084eacc35dfb6a055ba4bc9883807d3ffc
Ryan Ruckley <rruckley@gmail.com>
2023-09-25 12:07:08 +1000

    add spec fn

--- src/template/component.rs
+++ src/template/component.rs
@@ -2,18 +2,28 @@
 //! 
 
 use serde::{Deserialize,Serialize};
+use tmflib::tmf620::{product_specification::ProductSpecification, product_offering::ProductOffering};
 
 /// A Component Template defines how to build a component
 #[derive(Debug,Deserialize,Serialize)]
 pub struct ComponentTemplate {
     pub name : String,
+    pub component : Option<ProductOffering>,
 }
 
 impl ComponentTemplate {
     /// Create a new component template
     pub fn new(name : String) -> ComponentTemplate {
         ComponentTemplate { 
-            name 
+            name ,
+            component : None,
         }
+    }
+
+    /// Add specification to this component
+    pub fn with_specification(mut self, specification : ProductSpecification) -> ComponentTemplate {
+        self.component = Some(ProductOffering::new(self.name.clone())
+            .with_specification(specification));
+        self
     }
 }
