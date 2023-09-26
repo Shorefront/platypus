5fc84991b2a119d98bf5aa4920f8c2b6369fe1ec
Ryan Ruckley <rruckley@gmail.com>
2023-09-26 16:27:14 +1000

    template handlers

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use actix_web::middleware::Logger;
 use actix_web::{post,web,App, HttpResponse,HttpServer, Responder};
 
 use common::config::Config;
+use tmflib::tmf620::product_offering::ProductOffering;
 use tmflib::tmf620::product_specification::{ProductSpecification, ProductSpecificationCharacteristic};
 use tmflib::tmf620::tmf620_catalog_management::TMF620CatalogueManagement;
 use tmflib::tmf648::quote::Quote;
@@ -18,11 +19,33 @@ use crate::template::component::ComponentTemplate;
 //use crate::model::component::product::ProductComponent;
 use crate::template::product::ProductTemplate;
 
-#[post("/tmflib/tmf620/")]
-pub async fn tmf620_handler(
-
+#[post("/compose/template/product")]
+pub async fn template_product_handler(
+    body : web::Json<ProductTemplate>
 ) -> impl Responder {
-    HttpResponse::Ok()
+    let data = body.into_inner();
+
+    HttpResponse::Ok().json(data)
+}
+
+#[post("/compose/template/component")]
+pub async fn template_component_handler(
+    body : web::Json<ComponentTemplate>
+) -> impl Responder {
+    let data = body.into_inner();
+
+    HttpResponse::Ok().json(data)
+}
+
+#[post("/tmflib/tmf620/offer")]
+pub async fn tmf620_handler(
+    body : web::Json<ProductOffering>
+) -> impl Responder {
+    let data = body.into_inner();
+
+    // Since this is a create, we need to generate the Id / Href
+    let new_offer = ProductOffering::from(data);
+    HttpResponse::Ok().json(new_offer)
 }
 
 #[post("/tmflib/tmf648/quote")]
@@ -50,6 +73,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(tmf620_handler)
             .service(tmf648_create_handler)
+            .service(template_component_handler)
+            .service(template_product_handler)
             .wrap(Logger::default())
     })
         .bind(("0.0.0.0",8000))?
