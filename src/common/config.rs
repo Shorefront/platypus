e056d6c2e0b113939f64b7729c1b221824167157
Ryan Ruckley <rruckley@gmail.com>
2023-10-16 18:05:51 +1100

    cleanup

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,10 +1,22 @@
 //! Configuration Module
 //! 
 
+#[derive(Clone, Debug, Default)]
 pub struct Config {}
 
 impl Config {
     pub fn new() -> Config {
         Config {}
+    }
+    pub fn get(item: &str) -> Option<String> {
+        match item {
+
+            _ => Config::get_default(item),
+        }
+    }
+    pub fn get_default(item : &str) -> Option<String> {
+        match item {
+            _ => None,
+        }
     }
 }
