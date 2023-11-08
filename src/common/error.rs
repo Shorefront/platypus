c29c4e23610e617386290d88e9167cbaee4cc18e
Ryan Ruckley <rruckley@gmail.com>
2023-11-08 17:28:09 +1100

    TMFError

--- src/common/error.rs
+++ src/common/error.rs
@@ -24,3 +24,34 @@ impl From<&str> for PlatypusError {
        PlatypusError { message: value.to_owned() }
    }
 }
+
+#[derive(Default, Deserialize,Serialize)]
+pub struct TMFError {
+   code: String,
+   reason: String,
+   message: Option<String>,
+   status: Option<String>,
+   reference_error: Option<String>,
+}
+
+impl TMFError {
+   pub fn new(code : &str, reason : &str) -> TMFError {
+      TMFError { 
+         code: code.to_owned(), 
+         reason: reason.to_owned(), 
+         ..Default::default()
+      }
+   }
+}
+
+impl From<PlatypusError> for TMFError {
+   fn from(value: PlatypusError) -> Self {
+       TMFError { 
+         code: "PERR001".into(), 
+         reason: value.message, 
+         message: None, 
+         status: None, 
+         reference_error: None 
+      }
+   }
+}
