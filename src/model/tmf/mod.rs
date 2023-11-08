5b45b5a7ed27f93e4f15718b628a5cb6ee010ebe
Ryan Ruckley <rruckley@gmail.com>
2023-11-08 16:40:24 +1100

    move fn to persistence

--- src/model/tmf/mod.rs
+++ src/model/tmf/mod.rs
@@ -2,13 +2,9 @@
 //! 
 
 use surrealdb::sql::Thing;
-use surrealdb::Surreal;
-use surrealdb::engine::local::Db;
-use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use serde::{Deserialize, Serialize};
 
 use tmflib::HasId;
-
-use crate::common::error::PlatypusError;
 
 pub mod tmf620_catalog_management;
 pub mod tmf622_product_order_manaagement;
@@ -29,58 +25,5 @@ pub fn tmf_payload<'a, T : HasId + Serialize + Clone + Deserialize<'a>>(item : T
             id : item.get_id().into(),
         }),
         item,
-    }
-}
-
-/// Generate function to store into a db.
-pub async fn create_tmf_item<'a, T : HasId + Serialize + Clone + DeserializeOwned>(db : Surreal<Db>,mut item : T) -> Result<Vec<T>,PlatypusError> {
-    let class = T::get_class();
-    item.generate_id();
-    let payload = tmf_payload(item);
-    let insert_records : Vec<TMF<T>> = db.create(class).content(payload).await?;
-    let output = insert_records.into_iter().map(|tmf| {
-        tmf.item
-    }).collect();
-    Ok(output)
-}
-
-pub async fn get_tmf_items<T : HasId + Serialize + Clone + DeserializeOwned>(db : Surreal<Db>) -> Result<Vec<T>,PlatypusError> {
-    let insert_records : Vec<TMF<T>> = db.select(T::get_class()).await?;
-    let output = insert_records.into_iter().map(|tmf| {
-        tmf.item.clone()
-    }).collect();
-    Ok(output)
-}
-
-pub async fn get_tmf_item<T : HasId + Serialize + Clone + DeserializeOwned>(db : Surreal<Db>,id : String) -> Result<Vec<T>,PlatypusError> {
-    let query = format!("SELECT * FROM {}:{}",T::get_class(),id);
-    let mut output = db.query(query).await?;
-    let result : Vec<TMF<T>> = output.take(0)?;
-    let item = result.iter().map(|tmf| {
-        tmf.clone().item
-    }).collect();
-    Ok(item)
-}
-
-pub async fn patch_tmf_item<T : HasId + Serialize + Clone + DeserializeOwned>(db : Surreal<Db>, id : String, patch : String) -> Result<Vec<T>,PlatypusError> {
-    let resource = format!("({},{})",T::get_class(),id);
-    let result : Result<Vec<TMF<T>>,_> = db.update(resource)
-        .merge(patch).await;
-    match result {
-        Ok(r) => {
-            Ok(r.into_iter().map(|tmf| {
-                tmf.item
-            }).collect())
-        },
-        Err(e) => Err(PlatypusError::from(e))
-    }
-}
-
-pub async fn delete_tmf_item<T : HasId + Serialize + Clone + DeserializeOwned>(db : Surreal<Db>, id : String) -> Result<bool,PlatypusError> {
-    let resource = format!("({},{})",T::get_class(),id);
-    let output : Result<Vec<TMF<T>>,_> = db.clone().delete(resource).await;
-    match output {
-        Ok(_) => Ok(true),
-        Err(e) => Err(PlatypusError::from(e)),
     }
 }
