3d0b1daccfad7e439cb8dab7978c3227cf546e9f
Ryan Ruckley <rruckley@gmail.com>
2023-11-29 09:47:29 +1100

    todo comment

--- src/model/tmf/tmf629_customer_management.rs
+++ src/model/tmf/tmf629_customer_management.rs
@@ -22,6 +22,11 @@ impl TMF629CustomerManagement {
     }
 
     pub async fn add_customer(&mut self, customer : Customer) -> Result<Vec<Customer>,PlatypusError> {
+        // Need to handle customer code collisions here.
+        // Step1, try to retrieve a customer record using customer.code
+        // Step2, if found, generate a new code with an offset. Rinse and repeat until no collision.
+        //let result = self.persist.get_items_filter("item.code={}", query_opts)
+        // SQL = "SELECT * FROM customer WHERE item.characteristic.name == 'code'"
         self.persist.create_tmf_item(customer).await
     }
 
