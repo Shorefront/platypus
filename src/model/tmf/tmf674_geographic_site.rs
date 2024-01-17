a91f9cb8eeb0e6906de4a4070392578078427bad
Ryan Ruckley <rruckley@gmail.com>
2024-01-17 16:02:54 +1100

    base functions

--- src/model/tmf/tmf674_geographic_site.rs
+++ src/model/tmf/tmf674_geographic_site.rs
@@ -2,14 +2,32 @@
 //! 
 //! 
 
-use tmflib::{tmf632::individual::Individual, HasId};
+use tmflib::{HasId, tmf674::geographic_site::GeographicSite};
 use crate::common::{error::PlatypusError, persist::Persistence};
 
 use super::{tmf_payload,TMF};
 
+use crate::QueryOptions;
+
 use log::{debug,error};
 
 #[derive(Clone, Debug)]
-struct TMF674GeographicSite {
+pub struct TMF674GeographicSiteManagement {
     persist : Persistence,
+}
+
+impl TMF674GeographicSiteManagement {
+    pub fn new(persist : Persistence) -> TMF674GeographicSiteManagement {
+        TMF674GeographicSiteManagement {
+            persist,
+        }
+    }
+
+    pub async fn get_sites(&self, query_opts : QueryOptions) -> Result<Vec<GeographicSite>,PlatypusError> {
+        self.persist.get_items(query_opts).await
+    }
+
+    pub async fn add_site(&mut self, site : GeographicSite) -> Result<Vec<GeographicSite>,PlatypusError> {
+        self.persist.create_tmf_item(site).await
+    }
 }
