54707475c426df5f46cabd7db10a0cfe40ec13be
Ryan Ruckley <rruckley@gmail.com>
2024-03-19 15:37:53 +1100

    new() for ComponentTemplate

--- src/template/component.rs
+++ src/template/component.rs
@@ -2,15 +2,32 @@
 //! 
 
 use serde::{Deserialize,Serialize};
-use tmflib::tmf620::product_offering::ProductOffering;
+use tmflib::tmf620::{category::{Category,CategoryRef}, product_offering::ProductOffering};
 use std::convert::Into;
+use super::TEMPLATE_CATEGORY;
 
 /// A Component Template defines how to build a component
-#[derive(Clone,Debug,Deserialize,Serialize)]
+#[derive(Clone,Default,Debug,Deserialize,Serialize)]
 pub struct ComponentTemplate {
     pub name : String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub component : Option<ProductOffering>,
+}
+
+impl ComponentTemplate {
+    pub fn new(name : impl Into<String>) -> ComponentTemplate {
+        ComponentTemplate::default()
+            .name(name)
+    }
+
+    pub fn name(mut self, name : impl Into<String>) -> ComponentTemplate {
+        self.name = name.into();
+        let cat = Category::new(TEMPLATE_CATEGORY.into());
+        let offering = ProductOffering::new(self.name.clone())
+            .with_category(CategoryRef::from(&cat));
+        self.component = Some(offering);
+        self
+    }
 }
 
 impl Into<ProductOffering> for ComponentTemplate {
