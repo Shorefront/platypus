95d6cd9103c3239e7ac91e33a41618fe7fc6d1d4
Ryan Ruckley <rruckley@gmail.com>
2024-05-29 12:30:50 +1000

    Handles use configure()

---
+++ src/model/tmf/tmf632/mod.rs
@@ -0,0 +1,105 @@
+//! TMC632 Module
+//! 
+
+use std::sync::Mutex;
+use tmf632_party_management::TMF632PartyManagement;
+use actix_web::{get,post,web, HttpResponse, Responder};
+
+// TMFLIB
+use tmflib::tmf632::individual::Individual;
+use tmflib::tmf632::organization::Organization;
+use tmflib::HasId;
+
+use crate::common::error::PlatypusError;
+use crate::common::persist::Persistence;
+use crate::QueryOptions;
+
+#[cfg(feature = "tmf632_v4")]
+pub mod tmf632_party_management;
+
+#[get("/tmflib/tmf632/{object}")]
+pub async fn tmf632_list_handler(
+    path : web::Path<String>,
+    tmf632: web::Data<Mutex<TMF632PartyManagement>>,
+    query : web::Query<QueryOptions>,
+    persist: web::Data<Mutex<Persistence>>,
+) -> impl Responder {
+    let query_opts = query.into_inner();
+    let mut tmf632 = tmf632.lock().unwrap();
+    let persist = persist.lock().unwrap();
+    tmf632.persist(persist.clone());
+    match path.as_str() {
+        "individual" => {
+            let result = tmf632.get_individuals(query_opts).await;
+            match result {
+                Ok(v) => HttpResponse::Ok().json(v),
+                Err(e) => HttpResponse::BadRequest().json(e),
+            }   
+        },
+        "organization" => todo!(),
+        _ => HttpResponse::BadRequest().json(PlatypusError::from("TMF632: Invalid Object"))
+    }  
+}
+
+/// Get a specific object
+#[get("/tmf-api/partyManagement/v4/{object}/{id}")]
+pub async fn tmf632_get_handler(
+    path : web::Path<(String,String)>,
+    tmf632: web::Data<Mutex<TMF632PartyManagement>>,
+    query : web::Query<QueryOptions>,
+) -> impl Responder {
+    let (object,id) = path.into_inner();
+    let query_opts = query.into_inner();
+    match object.as_str() {
+        "individual" => {
+            let result = tmf632.lock().unwrap().get_individual(id,query_opts).await;
+            match result {
+                Ok(v) => HttpResponse::Ok().json(v),
+                Err(e) => HttpResponse::BadRequest().json(e),
+            }       
+        },
+        _ => HttpResponse::BadRequest().json(PlatypusError::from("TMF632: Invalid Object"))    
+    }
+}
+
+#[post("/tmflib/tmf632/{object}")]
+pub async fn tmf632_post_handler(
+    path : web::Path<String>,
+    raw: web::Bytes,
+    tmf632: web::Data<Mutex<TMF632PartyManagement>>,
+) -> impl Responder {
+    let object = path.into_inner();
+    let json = String::from_utf8(raw.to_vec()).unwrap();
+    match object.as_str() {
+        "individual" => {
+            // Create individual object
+            let mut individual : Individual = serde_json::from_str(json.as_str()).unwrap();
+            individual.generate_id();
+            let records = tmf632.lock().unwrap().add_individual(individual.clone()).await;
+            match records {
+                Ok(r) => HttpResponse::Ok().json(r),
+                Err(e) => HttpResponse::BadRequest().json(e),
+            } 
+        },
+        "organization" => {
+            let mut organization : Organization = serde_json::from_str(json.as_str()).unwrap();
+            organization.generate_id();
+            HttpResponse::Ok().json(organization)
+        }
+        _ => {
+            HttpResponse::BadRequest().json(PlatypusError::from("TMF632: Invalid Object"))
+        }
+    } 
+}
+
+pub fn config_tmf632(cfg: &mut web::ServiceConfig) {
+    // Place our configuration into cfg
+    // NB: Since we are adding via this method, we don't have access to persist class
+    // so we need to get access to that via web_data instead now.
+    let tmf632 = TMF632PartyManagement::new(None);
+    cfg
+        .service(tmf632_list_handler)
+        .service(tmf632_get_handler)
+        .service(tmf632_post_handler)
+        .app_data(web::Data::new(Mutex::new(tmf632.clone())));
+}
---
+++ src/model/tmf/tmf632/tmf632_party_management.rs
@@ -0,0 +1,66 @@
+//! Party Management Module
+
+use tmflib::{tmf632::individual::Individual, HasId};
+use crate::common::{error::PlatypusError, persist::Persistence};
+use crate::QueryOptions;
+
+use crate::model::tmf::{tmf_payload,TMF};
+
+use log::{debug,error};
+
+#[derive(Clone, Debug)]
+pub struct TMF632PartyManagement {
+    persist : Option<Persistence>,
+}
+
+impl TMF632PartyManagement {
+    pub fn new(persist : Option<Persistence>) -> TMF632PartyManagement {
+        TMF632PartyManagement {
+            persist,
+        }
+    }
+
+    pub fn persist(&mut self, persist: Persistence) {
+        self.persist = Some(persist);
+    }
+    fn party_exists(&self, party_id : String) -> Result<String,PlatypusError> {
+        // Confirm if the party exists in the DB
+        Ok(party_id)
+    }
+    fn validate_individual(&self, individual : &Individual) -> Result<bool,PlatypusError> {
+        let mut err_count = 0;
+        if individual.related_party.is_some() {
+            // There is some here, lets iterate and validate each related party
+            individual.related_party.as_ref().unwrap().into_iter().for_each(|rp| {
+                if self.party_exists(rp.id.clone()).is_err() {
+                    err_count += 1;
+                }
+            });
+        }
+        if err_count > 0 {
+            return Err(PlatypusError::from("TMF632: Invalid related party for individual"));
+        }
+        Ok(true) 
+    }
+    pub async fn add_individual(&mut self, individual : Individual) -> Result<Individual,PlatypusError> {
+        match self.validate_individual(&individual) {
+            Ok(_) => debug!("Individual validated"),
+            Err(e) => {
+                error!("Individual failed validation: {}",e);
+                return Err(e);
+            }
+        };
+        let payload = tmf_payload(individual);
+        let insert_records : Vec<TMF<Individual>> = self.persist.as_mut().unwrap().db.create(Individual::get_class()).content(payload).await?;
+        let record = insert_records.first().unwrap();
+        Ok(record.item.clone())
+    }
+
+    pub async fn get_individuals(&mut self,query_opts : QueryOptions) -> Result<Vec<Individual>,PlatypusError> {
+        self.persist.as_mut().unwrap().get_items(query_opts).await
+    }
+
+    pub async fn get_individual(&mut self, id : String, query_opts : QueryOptions) -> Result<Vec<Individual>,PlatypusError> {
+        self.persist.as_mut().unwrap().get_item(id,query_opts).await
+    }
+}
