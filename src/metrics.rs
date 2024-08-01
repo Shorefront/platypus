a08bd6ed44eca58ed9e2bf5bd5171d2bb7690e1d
Ryan Ruckley <rruckley@gmail.com>
2024-08-01 13:08:09 +1000

    moved to main

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,9 +1,9 @@
 //! Metrics module
 
-use std::collections::HashMap;
+
 
 use actix_web::{web,HttpResponse};
-use actix_web_prom::{PrometheusMetrics,PrometheusMetricsBuilder};
+// use actix_web_prom::PrometheusMetricsBuilder;
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -13,13 +13,8 @@ pub fn config_metrics(cfg: &mut web::ServiceConfig) {
     // Place our configuration into cfg
     // NB: Since we are adding via this method, we don't have access to persist class
     // so we need to get access to that via web_data instead now.
-    let mut labels = HashMap::new();
-    labels.insert("Application".to_string(),"Platypus".to_string());
-    let prom = PrometheusMetricsBuilder::new("api")
-        .endpoint("/metrics")
-        .const_labels(labels)
-        .build()
-        .unwrap();
+    
+
     cfg
         // .wrap(prom.clone())
         // .service(prom.clone())
