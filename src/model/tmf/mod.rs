eb0aa87a823b659aa7dc2cf8150aab0fc95200b2
Ryan <rruckley@gmail.com>
2024-10-27 16:28:56 +1100

    DB upates

--- src/model/tmf/mod.rs
+++ src/model/tmf/mod.rs
@@ -1,7 +1,6 @@
 //! TMF Modules
 //! 
 
-use surrealdb::sql::Thing;
 use serde::{Deserialize, Serialize};
 
 use tmflib::HasId;
@@ -73,17 +72,16 @@ pub fn render_post_output<T : Serialize + HasId>(output : Result<Vec<T>,Platypus
 /// Generic TMF struct for DB
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct TMF<T : HasId> {
-    id : Option<Thing>,
+    id : String,
+    class : String,
     pub item : T,
 }
 
 /// Geneate a TMF payload for storing in the database
 pub fn tmf_payload<'a, T : HasId + Serialize + Clone + Deserialize<'a>>(item : T) -> TMF<T> {
     TMF {
-        id : Some(Thing {
-            tb : T::get_class(),
-            id : item.get_id().into(),
-        }),
+        id : item.get_id(),
+        class : T::get_class(),
         item,
     }
 }
