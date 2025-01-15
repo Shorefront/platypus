27b7f8af094919dbdcbd959a5a98c4f1116a8f96
Ryan Ruckley <rruckley@gmail.com>
2025-01-15 17:47:08 +1100

    feat: Add DB config items

--- src/common/config.rs
+++ src/common/config.rs
@@ -4,6 +4,8 @@
 
 const PLATYPUS_PORT : &str = "8001";
 const PLATYPUS_WORKERS : u16 = 4;
+const DB_HOST : &str = "wss://platypus-06a3rhk0qlrtj092qq5dgtl91o.aws-use1.surreal.cloud";
+const DB_NS : &str = "tmf";
 
 
 #[derive(Clone, Debug, Default)]
@@ -23,6 +25,8 @@ impl Config {
         match item {
             "PLATYPUS_PORT" => Some(PLATYPUS_PORT.to_string()),
             "PLATYPUS_WORKERS" => Some(PLATYPUS_WORKERS.to_string()),
+            "DB_HOST" => Some(DB_HOST.to_string()),
+            "DB_NS" => Some(DB_NS.to_string()),
             _ => None,
         }
     }
