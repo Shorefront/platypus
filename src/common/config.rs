31895eda9886ad815be50457baf521c648674b03
Ryan Ruckley <rruckley@gmail.com>
2025-01-16 10:58:03 +1100

    bug: Now check env fist before using defaults

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,11 +1,14 @@
 //! Configuration Module
 //! 
 
+use std::env;
 
 const PLATYPUS_PORT : &str = "8001";
 const PLATYPUS_WORKERS : u16 = 4;
 const DB_HOST : &str = "wss://platypus-06a3rhk0qlrtj092qq5dgtl91o.aws-use1.surreal.cloud";
 const DB_NS : &str = "tmf";
+const DB_USER : &str = "platypus";
+const DB_PASS : &str = "Platypus2025!";
 
 
 #[derive(Clone, Debug, Default)]
@@ -16,8 +19,8 @@ impl Config {
         Config {}
     }
     pub fn get(&self, item: &str) -> Option<String> {
-        match item {
-
+        match env::var(item) {
+            Ok(e) => Some(e),
             _ => Config::get_default(item),
         }
     }
@@ -27,6 +30,8 @@ impl Config {
             "PLATYPUS_WORKERS" => Some(PLATYPUS_WORKERS.to_string()),
             "DB_HOST" => Some(DB_HOST.to_string()),
             "DB_NS" => Some(DB_NS.to_string()),
+            "DB_USER" => Some(DB_USER.to_string()),
+            "DB_PASS" => Some(DB_PASS.to_string()),
             _ => None,
         }
     }
