11b096a7c7b5c5357b21291fb71caf083c829eef
Ryan Ruckley <rruckley@gmail.com>
2025-01-16 16:48:03 +1100

    feat: Generate catalog events

--- src/model/tmf/tmf620/tmf620_catalog_management.rs
+++ src/model/tmf/tmf620/tmf620_catalog_management.rs
@@ -1,7 +1,8 @@
 //! TMF620 Catalog Management Module
 
+use tmflib::common::event::EventPayload;
 use tmflib::tmf620::category::Category;
-use tmflib::tmf620::catalog::Catalog;
+use tmflib::tmf620::catalog::{Catalog,CatalogEventType};
 use tmflib::tmf620::product_offering::ProductOffering;
 use tmflib::tmf620::product_offering_price::ProductOfferingPrice;
 use tmflib::tmf620::product_specification::ProductSpecification;
@@ -9,8 +10,6 @@ use tmflib::tmf620::product_specification::ProductSpecification;
 use crate::QueryOptions;
 
 use serde::{Deserialize,Serialize};
-
-use log::error;
 
 use surrealdb::sql::Thing;
 
@@ -44,7 +43,13 @@ impl TMF620CatalogManagement
     }
 
     pub async fn add_catalog(&mut self, catalog : Catalog) -> Result<Vec<Catalog>,PlatypusError> {
-        self.persist.as_mut().unwrap().create_tmf_item(catalog).await
+        let result = self.persist.as_mut().unwrap().create_tmf_item(catalog.clone()).await;
+        #[cfg(feature = "events")]
+        {
+            let event = catalog.to_event(CatalogEventType::CatalogCreateEvent);
+            let _new_event = self.persist.as_ref().unwrap().store_tmf_event(event).await?;
+        }
+        result
     }
 
     pub async fn add_specification(&mut self, mut specification: ProductSpecification) -> Result<Vec<ProductSpecification>,PlatypusError> {
@@ -63,22 +68,7 @@ impl TMF620CatalogManagement
     }
 
     pub async fn add_category(&mut self, mut category : Category) -> Result<Vec<Category>,PlatypusError> {
-        
-        if !category.root() && category.parent_id.is_some() {
-            let parent_id = category.parent_id.as_ref().unwrap().clone();
-            // Need to check if parentId is pointing to a valid parent
-            let parent_query = format!("SELECT * FROM category:{}",parent_id);
-            let mut parent_resp = self.persist.as_mut().unwrap().db.query(parent_query).await?;
-            let parent : Vec<CategoryRecord> = parent_resp.take(0).unwrap();
-            if parent.is_empty() {
-                // Throw error, parent not found
-                let msg = format!("ParentId {} not found for child {}",&parent_id,category.id.clone().unwrap());
-                error!("add_category: {msg}");
-                return Err(PlatypusError { message: msg, })
-            }
-        }
-
-        // Simiarly, if flagged as root, cannot also have parent_id
+        // If flagged as root, cannot also have parent_id
         if category.root() {
             category.parent_id = None;
         }
@@ -148,23 +138,32 @@ impl TMF620CatalogManagement
         self.persist.as_ref().unwrap().patch_tmf_item(id,patch).await
     }
 
-    pub async fn delete_category(&self, id : String) -> Result<bool, PlatypusError> {
+    pub async fn delete_category(&self, id : String) -> Result<Category, PlatypusError> {
         self.persist.as_ref().unwrap().delete_tmf_item::<Category>(id).await
     }
 
-    pub async fn delete_catalog(&self, id : String) -> Result<bool, PlatypusError> {
-        self.persist.as_ref().unwrap().delete_tmf_item::<Catalog>(id).await
+    pub async fn delete_catalog(&self, id : String) -> Result<Catalog, PlatypusError> {
+        let result = self.persist.as_ref().unwrap().delete_tmf_item::<Catalog>(id).await;
+        #[cfg(feature = "events")]
+        {
+            if result.is_ok() {
+                let catalog = result.as_ref().ok().unwrap();
+                let event = catalog.to_event(CatalogEventType::CatalogDeleteEvent);
+                let _new_event = self.persist.as_ref().unwrap().store_tmf_event(event).await?;
+            }
+        }
+        result
     }
 
-    pub async fn delete_specification(&self, id : String) -> Result<bool,PlatypusError> {
+    pub async fn delete_specification(&self, id : String) -> Result<ProductSpecification,PlatypusError> {
         self.persist.as_ref().unwrap().delete_tmf_item::<ProductSpecification>(id).await
     }
 
-    pub async fn delete_offering(&self, id : String) -> Result<bool, PlatypusError> {
+    pub async fn delete_offering(&self, id : String) -> Result<ProductOffering, PlatypusError> {
         self.persist.as_ref().unwrap().delete_tmf_item::<ProductOffering>(id).await
     }
     
-    pub async fn delete_price(&self, id : String) -> Result<bool, PlatypusError> {
+    pub async fn delete_price(&self, id : String) -> Result<ProductOfferingPrice, PlatypusError> {
         self.persist.as_ref().unwrap().delete_tmf_item::<ProductOfferingPrice>(id).await
     }
 }
