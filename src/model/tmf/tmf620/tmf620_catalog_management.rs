8a84d293d2665c0ab52aa9cd53244269c99fec97
Ryan <rruckley@gmail.com>
2025-01-20 15:26:08 +1100

    feat: Use generic fns that set lastUpdate

--- src/model/tmf/tmf620/tmf620_catalog_management.rs
+++ src/model/tmf/tmf620/tmf620_catalog_management.rs
@@ -43,7 +43,7 @@ impl TMF620CatalogManagement
     }
 
     pub async fn add_catalog(&mut self, catalog : Catalog) -> Result<Vec<Catalog>,PlatypusError> {
-        let result = self.persist.as_mut().unwrap().create_tmf_item(catalog.clone()).await;
+        let result = self.persist.as_mut().unwrap().create_tmf_item_lastupdate(catalog.clone()).await;
         #[cfg(feature = "events")]
         {
             let event = catalog.to_event(CatalogEventType::CatalogCreateEvent);
@@ -55,16 +55,16 @@ impl TMF620CatalogManagement
     pub async fn add_specification(&mut self, mut specification: ProductSpecification) -> Result<Vec<ProductSpecification>,PlatypusError> {
         // New record, needs appropriate status
         specification.status("New");
-        self.persist.as_mut().unwrap().create_tmf_item(specification).await
+        self.persist.as_mut().unwrap().create_tmf_item_lastupdate(specification).await
     }
 
     pub async fn add_offering(&mut self, mut offering : ProductOffering) -> Result<Vec<ProductOffering>,PlatypusError> {
         offering.status("New");
-        self.persist.as_mut().unwrap().create_tmf_item(offering).await
+        self.persist.as_mut().unwrap().create_tmf_item_lastupdate(offering).await
     }
 
     pub async fn add_price(&mut self, price: ProductOfferingPrice) -> Result<Vec<ProductOfferingPrice>,PlatypusError> {
-        self.persist.as_mut().unwrap().create_tmf_item(price).await
+        self.persist.as_mut().unwrap().create_tmf_item_lastupdate(price).await
     }
 
     pub async fn add_category(&mut self, mut category : Category) -> Result<Vec<Category>,PlatypusError> {
@@ -73,7 +73,7 @@ impl TMF620CatalogManagement
             category.parent_id = None;
         }
 
-        self.persist.as_mut().unwrap().create_tmf_item(category).await
+        self.persist.as_mut().unwrap().create_tmf_item_lastupdate(category).await
     }
 
     pub async fn get_catalogs(&self, query_opts : QueryOptions) -> Result<Vec<Catalog>,PlatypusError> {
@@ -119,23 +119,23 @@ impl TMF620CatalogManagement
     }
 
     pub async fn patch_category(&self, id : String, patch : Category) -> Result<Vec<Category>,PlatypusError> {
-        self.persist.as_ref().unwrap().patch_tmf_item(id, patch).await
+        self.persist.as_ref().unwrap().patch_tmf_item_lastupdate(id, patch).await
     }
 
     pub async fn patch_catalog(&self, id : String, patch : Catalog) -> Result<Vec<Catalog>,PlatypusError> {
-        self.persist.as_ref().unwrap().patch_tmf_item(id, patch).await
+        self.persist.as_ref().unwrap().patch_tmf_item_lastupdate(id, patch).await
     }
     
     pub async fn patch_specification(&self, id : String, patch : ProductSpecification) -> Result<Vec<ProductSpecification>,PlatypusError> {
-        self.persist.as_ref().unwrap().patch_tmf_item(id, patch).await
+        self.persist.as_ref().unwrap().patch_tmf_item_lastupdate(id, patch).await
     }
 
     pub async fn patch_offering(&self, id : String, patch : ProductOffering) -> Result<Vec<ProductOffering>, PlatypusError> {
-        self.persist.as_ref().unwrap().patch_tmf_item(id, patch).await
+        self.persist.as_ref().unwrap().patch_tmf_item_lastupdate(id, patch).await
     }
     
     pub async fn patch_price(&self, id : String, patch : ProductOfferingPrice) -> Result<Vec<ProductOfferingPrice>, PlatypusError> {
-        self.persist.as_ref().unwrap().patch_tmf_item(id,patch).await
+        self.persist.as_ref().unwrap().patch_tmf_item_lastupdate(id,patch).await
     }
 
     pub async fn delete_category(&self, id : String) -> Result<Category, PlatypusError> {
