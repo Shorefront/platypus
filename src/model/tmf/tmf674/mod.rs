c243f77cdc5847d919e8678a2c01e6b4dfe6bdeb
Ryan Ruckley <rruckley@gmail.com>
2025-01-21 09:05:04 +1100

    bug: Now use the correct render function for GET operation

--- src/model/tmf/tmf674/mod.rs
+++ src/model/tmf/tmf674/mod.rs
@@ -17,7 +17,7 @@ use actix_web::{
 };
 
 use crate::model::tmf::{
-    // render_get_output,
+    render_get_output,
     render_list_output,
     render_post_output,
     render_patch_output,
@@ -65,7 +65,7 @@ pub async fn tmf674_get_handler(
     match object.as_str() {
         "geographicSite" => {
             let customers = tmf674.get_site(id, query_opts).await;
-            render_list_output(customers)
+            render_get_output(customers)
         },
         _ => {
             HttpResponse::BadRequest().json(PlatypusError::from("Invalid Object"))   
